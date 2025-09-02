b1431b64c2122d931a27b9b2ec3638c5fcd35283
K <a24216884@alumnos.uady.mx>
2025-09-02 18:04:48 +0000

    secreo dos objetos nuevos

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -3,5 +3,8 @@ public class Main{
     public static void main(String[] args){
         Cliente cliente1 = new Cliente("AlanK", 190982);
         CuentaBancaria cuenta1 = new CuentaBancaria(89865,cliente1,100000);
+
+        Cliente cliente2 = new Cliente("lala", 1112244);
+        CuentaBancaria cuenta2 = new CuentaBancaria(93839, cliente2, 920283);
     }
 }
