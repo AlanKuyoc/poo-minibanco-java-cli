0207584e767c857d590f7390c6ea4dbb2ad945dd
K <a24216884@alumnos.uady.mx>
2025-09-02 18:13:27 +0000

    modificaciones en el objeto cuenta2

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -5,6 +5,6 @@ public class Main{
         CuentaBancaria cuenta1 = new CuentaBancaria(89865,cliente1,100000);
 
         Cliente cliente2 = new Cliente("lala", 1112244);
-        CuentaBancaria cuenta2 = new CuentaBancaria(93839, cliente2, 920283);
+        CuentaBancaria cuenta2 = new CuentaBancaria(93839,cliente2, 920283);
     }
 }
